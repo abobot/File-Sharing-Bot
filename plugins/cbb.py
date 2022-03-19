1124eb53cc0efdae374563f294a99dc122551ee3
阿波a-bo <32825442+abobot@users.noreply.github.com>
2022-03-19 13:56:23 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/abobot/File-Sharing-Bot'>Click here</a>\n○ Channel : @文件分享备份(频道)\n○ Support Group : @Something Backup</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/abobot/File-Sharing-Bot'>Click here</a>\n○ Channel : <a href='https://t.me/+LDTKn8G_1itlMjBl'>@文件分享备份(频道)</a>\n○ Bot : @filetolink1233bot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
